551eb9d556429cd0917625fe6b32cd6cf399267e
rostyslav-shyian-exa <rostyslav.shyian@exabeam.com>
2022-05-24 02:32:16 -0700

    added update to5 lesson

--- lesson2/task5/src/__tests__/Dialog.test.jsx
+++ lesson2/task5/src/__tests__/Dialog.test.jsx
@@ -3,20 +3,38 @@ import { shallow } from "enzyme";
 import Dialog from "../Dialog";
 import "jsdom-global/register";
 
+const mockFunction = jest.fn();
+
 describe("Dialog", () => {
   test("should not be show <Dialog />", () => {
-    const wrappedComponent = shallow(<Dialog isOpen={false} />);
+    const wrappedComponent = shallow(
+      <Dialog onClose={mockFunction} isOpen={false} />
+    );
     expect(wrappedComponent.find(".dialog").exists()).toBeFalsy();
   });
 
   test("should be show <Dialog />", () => {
-    const wrappedComponent = shallow(<Dialog isOpen />);
+    const wrappedComponent = shallow(<Dialog onClose={mockFunction} isOpen />);
     expect(wrappedComponent.find(".dialog").exists()).toBeTruthy();
   });
 
   test("should be closed <Dialog />", () => {
-    const wrappedComponent = shallow(<Dialog />);
+    const wrappedComponent = shallow(<Dialog onClose={mockFunction} isOpen />);
     wrappedComponent.find(".dialog__close-btn").simulate("click");
-    expext(wrappedComponent.find("Dialog").exists()).toBeFalsy();
+    expect(wrappedComponent.find(".dialog").exists()).toBeTruthy();
+  });
+
+  test("should be show h1", () => {
+    const wrappedComponent = shallow(
+      <Dialog onClose={mockFunction} isOpen title="Ivan" />
+    );
+    expect(wrappedComponent.find(".dialog__title").text()).toEqual("Ivan");
+  });
+
+  it("should be show children elements", () => {
+    const wrappedComponent = shallow(
+      <Dialog onClose={mockFunction} isOpen children="Grid Dynamics" />
+    );
+    expect(wrappedComponent.find(".dialog__content")).toMatchSnapshot();
   });
 });
